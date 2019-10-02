21af19f3e736c23931768b71a214ede111259802
e211320 <oleksander.dushyn@evry.com>
2019-10-02 23:34:59 +0200

    add insertion logic and new logos

--- src/main/kotlin/HelloAction.kt
+++ src/main/kotlin/HelloAction.kt
@@ -1,15 +1,17 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.vfs.VirtualFile
+import org.apache.commons.lang.StringUtils
 
 class HelloAction : AnAction("Hello") {
 
-    val AD_WIDTH = 5
-    val AD_HEIGHT = 10
+    private val AD_WIDTH = 5
+    private val AD_HEIGHT = 10
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project  = e.project
+        val project = e.project
         //Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon())
         val editor = e.getData(CommonDataKeys.EDITOR)
 
@@ -17,15 +19,23 @@ class HelloAction : AnAction("Hello") {
         val offset = editor?.caretModel?.offset
         println(offset)
 
-//        WriteCommandAction.runWriteCommandAction(project) {editor?.document?.insertString(offset!!, "Hello")}
-//        WriteCommandAction.runWriteCommandAction(project) {editor?.document?.replaceString(offset!! + 6, offset!! + 10,"")}
 
         val vir: VirtualFile? = e.getData(CommonDataKeys.VIRTUAL_FILE)
         //println((vir?.contentsToByteArray()?.let { String(it) }))
+        checkAdPlaceAvailability(vir)
 
-        vir?.contentsToByteArray()?.let { String(it) }!!.split("\n").forEachIndexed { index, el ->
-            print(el.indexOf(el.trim()))
-            print((el))
+        vir?.contentsToByteArray()?.let { String(it) }!!.split("\n").forEachIndexed { index, line ->
+            if (isLineAdable(line)) {
+                val lineOffset = editor?.document?.getLineStartOffset(index)
+                WriteCommandAction.runWriteCommandAction(project) {
+                    editor?.document?.insertString(lineOffset!!, "/*  _   _  ___  _   _ _ __  */")
+                    editor?.document?.replaceString(
+                        lineOffset!! + 31,
+                        lineOffset!! + 61,
+                        ""
+                    )
+                }
+            }
         }
 
 //        vir?.let {
@@ -38,7 +48,22 @@ class HelloAction : AnAction("Hello") {
 
     }
 
+    fun checkAdPlaceAvailability(vir: VirtualFile?) {
+        var availableAdHeight = 0
+        vir?.contentsToByteArray()?.let { String(it) }!!.split("\n").forEachIndexed { index, line ->
+            if (isLineAdable(line)) {
+                availableAdHeight++
+            } else {
+                availableAdHeight = 0
+            }
+
+            if (availableAdHeight == AD_HEIGHT) {
+                println("ADD AD!")
+            }
+        }
+    }
+
     fun isLineAdable(s: String): Boolean {
-        return s.indexOf(s.trim()) > 5
+        return StringUtils.isEmpty(s.trim()) || s.indexOf(s.trim()) > AD_WIDTH
     }
 }
